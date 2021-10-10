f1abe8a1b5423a1294ded76f31052271508a0031
Alejandro <janodevg@outlook.cl>
2021-10-10 00:35:15 -0300

    registrador de eventos para cuentas de usuarios

--- src/main/java/cl/gpsmain/datasource/service/core/ActivityService.java
+++ src/main/java/cl/gpsmain/datasource/service/core/ActivityService.java
@@ -1,7 +1,23 @@
 package cl.gpsmain.datasource.service.core;
 
+import cl.gpsmain.datasource.model.Account;
+import cl.gpsmain.datasource.model.Activity;
+import cl.gpsmain.datasource.service.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class ActivityService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public void logActivity(Account account, String activity, String description) {
+        account.getActivity().add(new Activity(LocalDateTime.now(), activity, description));
+        accountRepository.save(account);
+
+    }
+
 }
