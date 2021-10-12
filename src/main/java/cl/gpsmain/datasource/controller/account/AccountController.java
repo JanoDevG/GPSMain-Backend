350c31c95367c2086bec5c690de4f07b74499855
Alejandro <janodevg@outlook.cl>
2021-10-12 16:30:52 -0300

    registro de operaciones en CRUD de clietnes

--- src/main/java/cl/gpsmain/datasource/controller/account/AccountController.java
+++ src/main/java/cl/gpsmain/datasource/controller/account/AccountController.java
@@ -21,10 +21,9 @@ public class AccountController {
     public ResponseEntity<Response> accountController(@RequestHeader("X-option") String option,
                                                       @RequestHeader("X-ClientSecret") String clientSecret,
                                                       @RequestHeader("X-ClientId") String clientId,
-                                                      @RequestHeader("X-profileId") String profileId,
-                                                      @RequestHeader("X-userId") String userId,
+                                                      @RequestHeader("X-mail") String mail,
                                                       @RequestBody(required = false) Account account) {
-        return accountService.accountService(account, clientId, clientSecret, option, profileId, userId);
+        return accountService.accountService(account, clientId, clientSecret, option, mail);
 
     }
 
--- src/main/java/cl/gpsmain/datasource/service/AccountService.java
+++ src/main/java/cl/gpsmain/datasource/service/AccountService.java
@@ -1,6 +1,7 @@
 package cl.gpsmain.datasource.service;
 
 import cl.gpsmain.datasource.model.Account;
+import cl.gpsmain.datasource.model.Activity;
 import cl.gpsmain.datasource.model.Response;
 import cl.gpsmain.datasource.service.core.ActivityService;
 import cl.gpsmain.datasource.service.core.ValidationService;
@@ -10,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class AccountService {
@@ -28,20 +31,27 @@ public class AccountService {
 
     private static final Response RESPONSE = new Response();
 
-    public ResponseEntity<Response> accountService(Account account, String clientId, String clientSecret, String option, String profileId, String userId) {
-        if (validationService.validateClientSecret(clientSecret, clientId)) {
-            RESPONSE.setBody("el clientSecret no es válido para el clientId informado");
-            RESPONSE.setStatus(HttpStatus.UNAUTHORIZED);
-            return new ResponseEntity<>(RESPONSE, RESPONSE.getStatus());
-        }
-        Account acc = accountRepository.findByMailAndBusinessName(account.getMail(), account.getBusinessName());
+    public ResponseEntity<Response> accountService(Account account, String clientId, String clientSecret, String option, String mail) {
+        Account accountApplicant = accountRepository.findByMail(mail);
+        Account acc = accountRepository.findByMail(mail);
+        validations(clientId, clientSecret, accountApplicant, mail);
+        if (RESPONSE.getStatus().isError())
+            return new ResponseEntity<>(RESPONSE, RESPONSE.getStatus()); // capa de validaciones no aprobada se detiene flujo para enviar Response
         switch (option) {
             case "CREATE": // create account if not exist
                 if (acc != null) {
                     RESPONSE.setBody("La cuenta ya existe, se debe actualizar si se requiere modificar.");
                     RESPONSE.setStatus(HttpStatus.BAD_REQUEST);
                 } else {
+                    account.setBusinessId(accountApplicant.getBusinessId());
+                    account.setBusinessName(accountApplicant.getBusinessName());
+                    account.getActivity().add(new Activity(LocalDateTime.now(), "Creación de cuenta", "Cuenta nueva creada bajo el perfil de: ".concat(account.getProfile())));
                     accountRepository.insert(account);
+                    activityService.logActivity(accountApplicant, "Creación de cuenta nueva",
+                            "Se crea cuenta nueva para: "
+                                    .concat(account.getNames())
+                                    .concat(" con el perfil de: ")
+                                    .concat(account.getProfile()));
                     RESPONSE.setBody("La cuenta fue creada exitosamente.");
                     RESPONSE.setStatus(HttpStatus.OK);
                 }
@@ -50,9 +60,10 @@ public class AccountService {
                 if (acc == null) {
                     RESPONSE.setBody("La cuenta no existe. No hay datos que actualizar.");
                     RESPONSE.setStatus(HttpStatus.BAD_REQUEST);
-
                 } else {
                     accountRepository.save(account);
+                    activityService.logActivity(accountApplicant, "Actualización de cuenta",
+                            "Se actualiza cuenta para: ".concat(account.getNames()));
                     RESPONSE.setBody("Cuenta actualizada exitosamente.");
                     RESPONSE.setStatus(HttpStatus.OK);
                 }
@@ -63,17 +74,34 @@ public class AccountService {
                     RESPONSE.setStatus(HttpStatus.BAD_REQUEST);
                 } else {
                     accountRepository.delete(acc);
+                    activityService.logActivity(accountApplicant, "Eliminación de cuenta",
+                            "Se elimina la cuenta de: ".concat(account.getNames()));
+                    RESPONSE.setBody("Cuenta actualizada exitosamente.");
                     RESPONSE.setBody("La cuenta de: ".concat(acc.getNames()).concat(" fue eliminada exitosamente."));
                     RESPONSE.setStatus(HttpStatus.OK);
                 }
                 break;
             default:
-                RESPONSE.setBody("la operación: ".concat(option).concat(" no es válida (Header: X-option)."));
+                RESPONSE.setBody("la opción: ".concat(option).concat(" no es válida (Header: X-option)."));
                 RESPONSE.setStatus(HttpStatus.METHOD_NOT_ALLOWED);
                 break;
         }
         return new ResponseEntity<>(RESPONSE, RESPONSE.getStatus());
     }
 
-
+    private void validations(String clientId, String clientSecret, Account account, String mail) {
+        RESPONSE.setBody(null);
+        RESPONSE.setStatus(HttpStatus.OK);
+        if (validationService.validateClientSecret(clientSecret, clientId)) { // Se debe autorizar OAuth2.0
+            RESPONSE.setBody("el clientSecret no es válido para el clientId informado");
+            RESPONSE.setStatus(HttpStatus.UNAUTHORIZED);
+            return;
+        }
+        if (account == null) { // La cuenta X debe existir para proceder con cualquier operción
+            RESPONSE.setBody("El Mail informado no se encuentra registrado, no se permiten operaciones con cuentas no registradas previamente.");
+            RESPONSE.setStatus(HttpStatus.UNAUTHORIZED);
+            return;
+        }
+        //TODO supervisor solo puede actualziar y eliminar cuentas de su empresa (crear asume el código que es para su empresa)
+    }
 }
